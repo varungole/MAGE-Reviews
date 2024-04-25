a52b87d7f30ee74c9a552b180dec57eb5a6af094
Varun Gole <varungole1999@gmail.com>
2024-04-25 17:09:21 -0400

    creating frontend

--- frontend/src/components/Program-Courses/SoftwareCourses.jsx
+++ frontend/src/components/Program-Courses/SoftwareCourses.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function SoftwareCourses() {
+  const [courses, setCourses] = useState([]);
+  const URL = 'http://localhost:8080/api/courses/v1';
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+
+  const fetchCourses = async () => {
+    try {
+      const response = await axios.get(URL);
+      setCourses(response.data);
+    } catch (error) {
+      console.error('Error fetching reviews:', error);
+    }
+  };
+
   return (
     <div className='software-courses'>
-          <div className='software-courses-list'>
-           <div className='software-course-list-item'>
-            <h3>ENPM611</h3>
-            <p>Professor: Christopher Ackermann</p>
-            <p>TA: </p>
-            <p>Grader: </p>
-            <p>Reviews</p>
-           </div>
-        </div>
+      <div className='software-courses-list'>
+        {courses.map((course, index) => (
+          <div key={index} className='course'>
+            <h2 className='course-name'>{course.courseName}</h2>
+            <p className='professor'>Professor: {course.professor}</p>
+            <p className='TA'>TA: {course.ta}</p>
+            <p className='grader'>Grader: {course.grader}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default SoftwareCourses
+export default SoftwareCourses;
