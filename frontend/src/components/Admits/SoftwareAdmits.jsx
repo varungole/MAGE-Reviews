92accb73b61f77ada08b0d5e4fe7dc7f3e94d444
Varun Gole <varungole1999@gmail.com>
2024-05-03 18:20:41 -0400

    frontend done

--- frontend/src/components/Admits/SoftwareAdmits.jsx
+++ frontend/src/components/Admits/SoftwareAdmits.jsx
@@ -1,15 +1,47 @@
-  import React, { useState } from 'react';
-
+  import React, { useEffect, useState } from 'react';
+  import axios from 'axios';
+  import { useNavigate } from 'react-router-dom';
+  
   function SoftwareAdmits() {
-
-    const handleSubmit = () => {
-
-    }
-
-    const [GRE, setGRE] = useState('');
-    const [TOEFL, setTOEFL] = useState('');
+  
+    const URL = "http://localhost:8080/api/scores/v1";
+    const navigate = useNavigate();
+    const [gre, setGRE] = useState('');
+    const [toefl, setTOEFL] = useState('');
     const [workex, setWorkex] = useState('');
     const [name, setName] = useState('');
+
+
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      const programId = 1;
+
+      const scoreData = {gre, toefl, workex, name, programId};
+
+      axios.post(URL, scoreData)
+      .then(response => {
+        console.log(response.data)
+        navigate('/software')
+      })
+      .catch(error => {
+        console.log(error.message)
+      })
+    }
+
+
+    const [scores, getScores] = useState([]);
+    useEffect(() => {
+      fetchScores();
+    }, []);
+    const fetchScores = async () => {
+      try {
+        const response = await axios.get(URL);
+        getScores(response.data);
+      } catch (error) {
+        console.error('Error fetching reviews:', error);
+      }
+    };
 
     
     return (
@@ -17,26 +49,16 @@
         <h2>Software Admissions</h2>
 
         <div className='admits-items'>
-          <div className='admit-item'>
-              <h3 className='admit-name'>Name</h3>
-              <h3 className='TOEFL'>108</h3>
-              <h3 className='GRE'>322</h3>
-          </div>
-          <div className='admit-item'>
-              <h3 className='admit-name'>Name</h3>
-              <h3 className='TOEFL'>107</h3>
-              <h3 className='GRE'>309</h3>
-          </div>
-          <div className='admit-item'>
-              <h3 className='admit-name'>Name</h3>
-              <h3 className='TOEFL'>107</h3>
-              <h3 className='GRE'>311</h3>
-          </div>
-          <div className='admit-item'>
-              <h3 className='admit-name'>Name</h3>
-              <h3 className='TOEFL'>109</h3>
-              <h3 className='GRE'>315</h3>
-          </div>
+         {
+          scores.map((score, index) => (
+            <div key={index} className='score-template'>
+              <h3 className='score-name'>{score.name}</h3>
+              <p className='gre-score'>GRE: {score.gre}</p>
+              <p className='toefl-score'>TOEFL: {score.toefl}</p>
+              <p className='workex'>Work Experience: {score.workex}</p>
+            </div>
+          ))
+         }
 
         </div>
         <div className='score'>
@@ -46,10 +68,10 @@
             <input className='name-input' required type='text' value={name} onChange={(e) => setName(e.target.value)}></input>
 
             <label className='gre'>GRE</label>
-            <input className='GRE-Input' required type='text' value={GRE} onChange={(e) => {setGRE(e.target.value)}}></input>
+            <input className='GRE-Input' required type='text' value={gre} onChange={(e) => {setGRE(e.target.value)}}></input>
 
             <label className='Toefl'>TOEFL</label>
-            <input className='toefl-input' required type='text' value={TOEFL} onChange={(e) => {setTOEFL(e.target.value)}}></input>
+            <input className='toefl-input' required type='text' value={toefl} onChange={(e) => {setTOEFL(e.target.value)}}></input>
 
             <label className='Workex' required type='text' value={workex} onChange={(e) => {setWorkex(e.target.value)}}>Enter your workex years</label>
             <input className='toefl-input' type='text' required value={workex} onChange={(e) => {setWorkex(e.target.value)}}></input>
